bdefd7d5ecf994926c12e47c1a6d410d28935908
melifluo89 <andreslkjhg@gmail.com>
2022-06-09 19:05:02 -0300

    first commit

---
+++ Migrations/20220609154632_Rating.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcGame.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Game",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Game");
+        }
+    }
+}
